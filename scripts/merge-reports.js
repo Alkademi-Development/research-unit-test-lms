45e75db96ca8d43a9d077b769efd12aa824e43d4
AdnanErlansyah <adnanerlansyah403@gmail.com>
2023-06-20 08:17:19 +0700

    updated merge reports

--- scripts/merge-reports.js
+++ scripts/merge-reports.js
@@ -3,22 +3,8 @@ import clc from "cli-color";
 import { merge } from 'mochawesome-merge';
 import { create } from 'mochawesome-report-generator';
 import { rl } from '#root/commons/utils/inputUtils';
-import { TEXT_REPORTS } from '#root/commons/constants/input';
+import { TEXT_REPORTS, TEXT_CONFIRM } from '#root/commons/constants/input';
 import moment from 'moment-timezone';
-
-async function mergeReports(directory, outputFile) {
-    const files = await getJsonFiles(directory);
-
-    if(files.length > 0) {
-      const reports = await merge({ files });
-    
-      fs.writeFileSync(outputFile, JSON.stringify(reports, null, 2));
-      console.log(clc.green(`Merged report saved to ${outputFile} 😊`));
-
-    } else {
-      console.log(`No reports saved to ${directory} 😢`);
-    }
-}
   
 function getJsonFiles(directory) {
     return new Promise((resolve, reject) => {
@@ -55,39 +41,131 @@ function askMergeReport() {
         console.log(clc.red('Input tidak valid, tolong masukkan sesuai instruksi'));
         askMergeReport();
       } else if(input.trim().toLowerCase() === 'app') {
+        
+        const directory = './testResults/reports/app';
+        
+        let files = await getJsonFiles(directory);
 
-        await mergeReports('./testResults/reports/app', `./testResults/output-merged-report-${input}.json`);
+        if(files.length > 0) {
 
-        if(fs.existsSync(`./testResults/output-merged-report-${input}.json`)) {
-          const reportData = fs.readFileSync(`./testResults/output-merged-report-${input}.json`, 'utf-8');
+          function askQuestion() {
+            rl.question(`Apakah anda ingin merge semua hasil report termasuk yang gagal atau yang berhasil saja? (Y/t) `, async inputConfirm => {
+              
+              if(!TEXT_CONFIRM.includes(inputConfirm)) {
+                console.log(clc.yellow(`Maaf, input tidak valid. Tolong masukkan sesuai instruksi`));
+                askQuestion();
+              } else if(inputConfirm.trim().toLowerCase() === 'y') {
+                const reports = await merge({ files });
+                const outputFile = `./testResults/output-merged-report-${input}.json`;  
+
+                fs.writeFileSync(outputFile, JSON.stringify(reports, null, 2));
+                
+                if(fs.existsSync(outputFile)) {
+                  const reportData = fs.readFileSync(outputFile, 'utf-8');
+          
+                  const options = { 
+                      reportPageTitle: `Report Testing ${input} ${moment().tz('Asia/Jakarta').format('MM-DD-YYYY')}`,
+                      reportDir: './testResults', // Path folder untuk menyimpan report
+                      reportFilename: `merged-report-${input}.html`, // Nama file report
+                      assetsDir: './testResults/assets', // Path folder untuk menyimpan assets
+                  };
+                  await create(JSON.parse(reportData), options);
+                  console.log(clc.green(`Merged reports successfully generated in './testResults/output-merged-report-${input}.html' 😊`))
+                  rl.close();
+                }
+              } else {
+                files = files.filter(file => !file.trim().toLowerCase().includes('failed'));
   
-          const options = { 
-              reportPageTitle: `Report Testing ${input} ${moment().tz('Asia/Jakarta').format('MM-DD-YYYY')}`,
-              reportDir: './testResults', // Path folder untuk menyimpan report
-              reportFilename: `merged-report-${input}.html`, // Nama file report
-              assetsDir: './testResults/assets', // Path folder untuk menyimpan assets
-          };
-          create(JSON.parse(reportData), options);
-        }
+                const reports = await merge({ files });
+                const outputFile = `./testResults/output-merged-report-${input}.json`;  
+                  
+                fs.writeFileSync(outputFile, JSON.stringify(reports, null, 2));
+                
+                if(fs.existsSync(outputFile)) {
+                  const reportData = fs.readFileSync(outputFile, 'utf-8');
+          
+                  const options = { 
+                      reportPageTitle: `Report Testing ${input} ${moment().tz('Asia/Jakarta').format('MM-DD-YYYY')}`,
+                      reportDir: './testResults', // Path folder untuk menyimpan report
+                      reportFilename: `merged-report-${input}.html`, // Nama file report
+                      assetsDir: './testResults/assets', // Path folder untuk menyimpan assets
+                  };
+                  await create(JSON.parse(reportData), options);
+                  console.log(clc.green(`Merged reports successfully generated in './testResults/output-merged-report-${input}.html' 😊`));
+                  rl.close();
+                } 
+              }
+  
+            })
+          }
 
-        rl.close();
+          askQuestion();
+
+        } else {
+          console.log(`No reports saved to ${directory} 😢`);
+          rl.close();
+        }
       } else if (input.trim().toLowerCase() === 'api') {
 
-        await mergeReports('./testResults/reports/api', `./testResults/output-merged-report-${input}.json`);
+        const directory = './testResults/reports/api';
+        
+        let files = await getJsonFiles(directory);
 
-        if(fs.existsSync(`./testResults/output-merged-report-${input}.json`)) {
-          const reportData = fs.readFileSync(`./testResults/output-merged-report-${input}.json`, 'utf-8');
+        if(files.length > 0) {
+          
+          function askQuestion() {
+            rl.question(`Apakah anda ingin merge semua hasil report termasuk yang gagal atau yang berhasil saja? (Y/t) `, async inputConfirm => {
+              
+              if(!TEXT_CONFIRM.includes(inputConfirm)) {
+                console.log(clc.yellow(`Maaf, input tidak valid. Tolong masukkan sesuai instruksi`));
+                askQuestion();
+              } else if(inputConfirm.trim().toLowerCase() === 'y') {
+                const reports = await merge({ files });
+                  
+                fs.writeFileSync(outputFile, JSON.stringify(reports, null, 2));
+                
+                if(fs.existsSync(`./testResults/output-merged-report-${input}.json`)) {
+                  const reportData = fs.readFileSync(`./testResults/output-merged-report-${input}.json`, 'utf-8');
+          
+                  const options = { 
+                      reportPageTitle: `Report Testing ${input} ${moment().tz('Asia/Jakarta').format('MM-DD-YYYY')}`,
+                      reportDir: './testResults', // Path folder untuk menyimpan report
+                      reportFilename: `merged-report-${input}.html`, // Nama file report
+                      assetsDir: './testResults/assets', // Path folder untuk menyimpan assets
+                  };
+                  await create(JSON.parse(reportData), options);
+                  console.log(clc.green(`Merged reports successfully generated in './testResults/output-merged-report-${input}.html' 😊`))
+                  rl.close();
+                } 
+              } else {
+                files = files.filter(file => !file.trim().toLowerCase().includes('failed'));
   
-          const options = { 
-              reportPageTitle: `Report Testing ${input} ${moment().tz('Asia/Jakarta').format('MM-DD-YYYY')}`,
-              reportDir: './testResults', // Path folder untuk menyimpan report
-              reportFilename: `merged-report-${input}.html`, // Nama file report
-              assetsDir: './testResults/assets', // Path folder untuk menyimpan assets
-          };
-          create(JSON.parse(reportData), options);
+                const reports = await merge({ files });
+                  
+                fs.writeFileSync(outputFile, JSON.stringify(reports, null, 2));
+                
+                if(fs.existsSync(`./testResults/output-merged-report-${input}.json`)) {
+                  const reportData = fs.readFileSync(`./testResults/output-merged-report-${input}.json`, 'utf-8');
+          
+                  const options = { 
+                      reportPageTitle: `Report Testing ${input} ${moment().tz('Asia/Jakarta').format('MM-DD-YYYY')}`,
+                      reportDir: './testResults', // Path folder untuk menyimpan report
+                      reportFilename: `merged-report-${input}.html`, // Nama file report
+                      assetsDir: './testResults/assets', // Path folder untuk menyimpan assets
+                  };
+                  await create(JSON.parse(reportData), options);
+                  console.log(clc.green(`Merged reports successfully generated in './testResults/output-merged-report-${input}.html' 😊`))
+                  rl.close();
+                } 
+              }
+  
+            })
+          }
+          askQuestion();
+        } else {
+          console.log(`No reports saved to ${directory} 😢`);
+          rl.close()
         }
-
-        rl.close();
       } else {
         console.log(clc.red('Input tidak valid, tolong masukkan sesuai instruksi'));
         askMergeReport();
